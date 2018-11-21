7166fb770269edf1f559cf857bb1a8d875d8bda9
Lashaunna Samuels <LSamuels_local@blueberry.corp.enterprise.local>
2018-11-21 15:58:03 -0500

    bookmark under componentDidMount

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ class App extends Component {
   componentDidMount() {
     this.onFetchFromGitHub()
   }
-
+// page 52
   onChange = event => {
     this.setState({ path: event.target.value })
   }
